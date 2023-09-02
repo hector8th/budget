b2965dde1c031485a73d18d1c087ab8aa352dd41
hector8th <115110070+hector8th@users.noreply.github.com>
2023-09-02 03:07:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,40 +22,44 @@ class App extends Component {
     const totalIncome = parseFloat(paycheckAmount) * parseInt(payFrequency);
     const newBalance = parseFloat(accountBalance) + totalIncome;
     const moneySaved = newBalance * (parseInt(budgetType) / 10);
-    const payDiff = (newBalance - moneySaved).toFixed(2);
+    // const payDiff = (newBalance - moneySaved).toFixed(2);
     const balanceDiff = newBalance - bills;
     let newAdvice = '';
 
+    // TWEAK THIS SO THAT IT SHOWS YOUR BALANCE, THE REMAINDER AFTER BILLS, AND HOW MUCH OF THAT REMAINDER IS FOR SPENDING
     if (budgetType === '3') {
-      if ((payFrequency >= 1) && (moneySaved > bills)) {
+      if ((payFrequency >= 1) && (moneySaved >= bills)) {
         newAdvice = `Light Budget(30%)!
-        \nYour Balance before your Bills/Expenses is $${newBalance}, so you can spend up to $${payDiff}!`;
-      } else if ((payFrequency >= 1) && (moneySaved <= bills)) {
+        \nYour Balance before your Bills/Expenses is $${newBalance}!
+        \nYou will be left with $${balanceDiff}, so you can spend up to $${(balanceDiff * 0.7).toFixed(2)}!`;
+      } else if ((payFrequency >= 1) && (moneySaved < bills)) {
         newAdvice = `SLOW DOWN!!!\nYou need to save your money!!!
-          \nYour Balance($${newBalance}) will not survive on this budget considering your Bills($${bills}) are more than what you saved!!!
-          \nAt 30%, your Balance will be $${balanceDiff.toFixed(2)} after your Bills are due!!!`;
+          \nYour Balance($${newBalance})(100%) will not survive on this budget considering your Bills($${bills}) are more than what you can save!!!
+          \nAt 30%, your Balance will be $${balanceDiff.toFixed(2)}(${((balanceDiff / newBalance) * 100).toFixed(0)}%) after your Bills are due!!!`;
       } else {
         newAdvice = `Please enter a correct number for Times Paid.`;
       }
     } else if (budgetType === '5') {
-      if ((payFrequency >= 1) && (moneySaved > bills)) {
+      if ((payFrequency >= 1) && (moneySaved >= bills)) {
         newAdvice = `Normal Budget(50%)!
-        \nYour Balance before your Bills/Expenses is $${newBalance}, so you can spend up to $${payDiff}!`;
-      } else if ((payFrequency >= 1) && (moneySaved <= bills)) {
+        \nYour Balance before your Bills/Expenses is $${newBalance}!
+        \nYou will be left with $${balanceDiff}, so you can spend up to $${(balanceDiff * 0.5).toFixed(2)}!`;
+      } else if ((payFrequency >= 1) && (moneySaved < bills)) {
         newAdvice = `SLOW DOWN!!!\nYou need to save your money!!!
-          \nYour Balance($${newBalance}) will not survive on this budget considering your Bills($${bills}) are more than what you saved!!!
-          \nAt 50%, your Balance will be $${balanceDiff.toFixed(2)} after your Bills are due!!!`;
+          \nYour Balance($${newBalance})(100%) will not survive on this budget considering your Bills($${bills}) are more than what you can save!!!
+          \nAt 50%, your Balance will be $${balanceDiff.toFixed(2)}(${((balanceDiff / newBalance) * 100).toFixed(0)}%) after your Bills are due!!!`;
       } else {
         newAdvice = `Please enter a correct number for Times Paid.`;
       }
     } else if (budgetType === '7') {
-      if ((payFrequency >= 1) && (moneySaved > bills)) {
+      if ((payFrequency >= 1) && (moneySaved >= bills)) {
         newAdvice = `Strict Budget(70%)!
-        \nYour Balance before your Bills/Expenses is $${newBalance}, so you can spend up to $${payDiff}!`;
-      } else if ((payFrequency >= 1) && (moneySaved <= bills)) {
+        \nYour Balance before your Bills/Expenses is $${newBalance}!
+        \nYou will be left with $${balanceDiff}, so you can spend up to $${(balanceDiff * 0.3).toFixed(2)}!`;
+      } else if ((payFrequency >= 1) && (moneySaved < bills)) {
         newAdvice = `SLOW DOWN!!!\nYou need to save your money!!!
-          \nYour Balance($${newBalance}) will not survive on this budget considering your Bills($${bills}) are more than what you saved!!!
-          \nAt 70%, your Balance will be $${balanceDiff.toFixed(2)} after your Bills are due!!!`;
+          \nYour Balance($${newBalance})(100%) will not survive on this budget considering your Bills($${bills}) are more than what you can save!!!
+          \nAt 70%, your Balance will be $${balanceDiff.toFixed(2)}(${((balanceDiff / newBalance) * 100).toFixed(0)}%) after your Bills are due!!!`;
       } else {
         newAdvice = `Please enter a correct number for Times Paid.`;
       }
@@ -77,14 +81,14 @@ class App extends Component {
           <h5>(If you'd like to simply calculate a paycheck, please put zero(0) in both Account Balance and Bills and Expenses Amount.)</h5>
           <div className="result">{advice}</div>
           <form className="form" onSubmit={this.handleSubmit}>
-            <label>Account Balance: $</label>
-            <input type="text" placeholder='0' value={accountBalance} onChange={(e) => this.setState({ accountBalance: e.target.value })} required />
-            <label>Paycheck Amount: $</label>
-            <input type="text" placeholder='0' value={paycheckAmount} onChange={(e) => this.setState({ paycheckAmount: e.target.value })} required />
-            <label>Times Paid Before Due Date:</label>
+            <label>Account Balance:</label>
+            <input type="text" placeholder='0.00' value={accountBalance} onChange={(e) => this.setState({ accountBalance: e.target.value })} required />
+            <label>Paycheck Amount:</label>
+            <input type="text" placeholder='0.00' value={paycheckAmount} onChange={(e) => this.setState({ paycheckAmount: e.target.value })} required />
+            <label># of Times Paid Before Due Date:</label>
             <input type="text" placeholder='1' value={payFrequency} onChange={(e) => this.setState({ payFrequency: e.target.value })} required />
             <label>Bills and Expenses Amount:</label>
-            <input type="text" placeholder='0' value={bills} onChange={(e) => this.setState({ bills: e.target.value })} required />
+            <input type="text" placeholder='0.00' value={bills} onChange={(e) => this.setState({ bills: e.target.value })} required />
             <label>Budget Type:</label>
             <select value={budgetType} onChange={(e) => this.setState({ budgetType: e.target.value })} required>
               <option value="1">---</option>
